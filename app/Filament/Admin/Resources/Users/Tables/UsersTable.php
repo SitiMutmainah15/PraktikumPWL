6557d81430adf6eb060e632ad5671980960b4f24
SitiMutmainah15 <iinsmk1rbg@gmail.com>
2026-03-05 22:32:49 +0700

    filament unik email, pw min 6, ubah icon done

---
+++ app/Filament/Admin/Resources/Users/Tables/UsersTable.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Filament\Admin\Resources\Users\Tables;
+
+use Filament\Tables\Table;
+use Filament\Tables\Columns\TextColumn;
+
+class UsersTable
+{
+    public static function configure(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')
+                    ->searchable()
+                    ->sortable(),
+
+                TextColumn::make('email')
+                    ->searchable(),
+
+                TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable(),
+            ]);
+    }
+}
--- app/Filament/Admin/Resources/Users/UserResource.php
+++ app/Filament/Admin/Resources/Users/UserResource.php
@@ -1,26 +1,24 @@
 <?php
 
-namespace App\Filament\Resources\Users;
+namespace App\Filament\Admin\Resources\Users;
 
-use App\Filament\Resources\Users\Pages\CreateUser;
-use App\Filament\Resources\Users\Pages\EditUser;
-use App\Filament\Resources\Users\Pages\ListUsers;
-use App\Filament\Resources\Users\Schemas\UserForm;
-use App\Filament\Resources\Users\Tables\UsersTable;
+use App\Filament\Admin\Resources\Users\Pages\CreateUser;
+use App\Filament\Admin\Resources\Users\Pages\EditUser;
+use App\Filament\Admin\Resources\Users\Pages\ListUsers;
+use App\Filament\Admin\Resources\Users\Schemas\UserForm;
+use App\Filament\Admin\Resources\Users\Tables\UsersTable;
 use App\Models\User;
 use BackedEnum;
 use Filament\Resources\Resource;
 use Filament\Schemas\Schema;
-use Filament\Support\Icons\Heroicon;
 use Filament\Tables\Table;
+use Filament\Support\Icons\Heroicon;
 
 class UserResource extends Resource
 {
     protected static ?string $model = User::class;
 
-    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
-
-    protected static ?string $recordTitleAttribute = 'name';
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::User;
 
     public static function form(Schema $schema): Schema
     {
@@ -30,13 +28,6 @@ class UserResource extends Resource
     public static function table(Table $table): Table
     {
         return UsersTable::configure($table);
-    }
-
-    public static function getRelations(): array
-    {
-        return [
-            //
-        ];
     }
 
     public static function getPages(): array
