8d0651f705ee04e2d8b5695c54970bf147c21784
SitiMutmainah15 <iinsmk1rbg@gmail.com>
2026-03-06 18:35:27 +0700

    filament 4

--- app/Filament/Admin/Resources/Posts/Schemas/PostForm.php
+++ app/Filament/Admin/Resources/Posts/Schemas/PostForm.php
@@ -6,10 +6,12 @@ use Filament\Forms\Components\TextInput;
 use Filament\Schemas\Schema;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\ColorPicker;
-use Filament\Forms\Components\MarkdownEditor;
 use Filament\Forms\Components\RichEditor;
 use Filament\Forms\Components\FileUpload;
-
+use Filament\Forms\Components\TagsInput;
+use Filament\Forms\Components\Checkbox;
+use Filament\Forms\Components\DateTimePicker;
+use Illuminate\Validation\Rules\Unique;
 
 class PostForm
 {
@@ -17,19 +19,34 @@ class PostForm
     {
         return $schema
             ->components([
-                //
-                TextInput::make("title"),
-                TextInput::make("slug"),
-                Select::make("category_id")
-                ->relationship("category", "name")
-                ->preload()
-                ->searchable(),
-                ColorPicker::make("color"),
-                //MarkdownEditor::make("content"),
-                RichEditor::make("content"),
-                FileUpload::make("Image")
-                ->disk("public")
-                ->directory("posts"),
+                TextInput::make('title')
+                    ->required()
+                    ->minLength(5),
+
+                TextInput::make('slug')
+                    ->required()
+                    ->unique(ignoreRecord: true),
+
+                Select::make('category_id')
+                    ->relationship('category', 'name')
+                    ->preload()
+                    ->searchable()
+                    ->required(),
+
+                ColorPicker::make('color'),
+
+                RichEditor::make('body'),
+
+                FileUpload::make('image')
+                    ->disk('public')
+                    ->directory('posts')
+                    ->image(),
+
+                TagsInput::make('tags'),
+
+                Checkbox::make('published'),
+
+                DateTimePicker::make('published_at'),
             ]);
     }
 }
