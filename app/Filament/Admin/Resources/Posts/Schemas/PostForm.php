9898a25337fc0f151a91fcdc5ea31a4de0a7522d
SitiMutmainah15 <iinsmk1rbg@gmail.com>
2026-03-06 14:19:36 +0700

    impelementasi form element

---
+++ app/Filament/Admin/Resources/Posts/Schemas/PostForm.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\Admin\Resources\Posts\Schemas;
+
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Schema;
+
+class PostForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                //
+                TextInput::make("title"),
+                TextInput::make("slug"),
+            ]);
+    }
+}
