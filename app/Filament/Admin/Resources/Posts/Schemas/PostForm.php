bed6a1d3e77798d87c01958298c3bbed15db38be
SitiMutmainah15 <iinsmk1rbg@gmail.com>
2026-03-08 01:13:59 +0700

    jobsheet 6 filament

--- app/Filament/Admin/Resources/Posts/Schemas/PostForm.php
+++ app/Filament/Admin/Resources/Posts/Schemas/PostForm.php
@@ -29,17 +29,30 @@ class PostForm
                                 Group::make([
                                     TextInput::make('title')
                                         ->required()
-                                        ->minLength(5),
+                                        ->minLength(5)
+                                         ->validationMessages([
+                                            'required' => 'Title wajib diisi.',
+                                            'minLength' => 'Title minimal 5 karakter.',
+                                        ]),
+                                    //->rules(['required', 'min:3', 'max:10'])
 
                                     TextInput::make('slug')
                                         ->required()
-                                        ->unique(ignoreRecord: true),
+                                        ->unique()
+                                        ->minLength(3)
+                                        ->validationMessages([
+                                            'unique' => 'Slug harus unik dan tidak boleh sama.',
+                                            'minLength'=> 'Slug minimal 3 karakter',
+                                        ]),
 
                                     Select::make('category_id')
                                         ->relationship('category', 'name')
                                         ->preload()
                                         ->searchable()
-                                        ->required(),
+                                        ->required()
+                                        ->validationMessages([
+                                            'required' => 'Category wajib dipilih.',
+                                        ]),
 
                                     ColorPicker::make('color'),
                                 ])->columns(2),
@@ -48,15 +61,18 @@ class PostForm
                                     ->columnSpanFull(),
                             ])
                             ->columnSpan(8),
-
                         Group::make([
                             Section::make('Image Upload')
                                 ->icon('heroicon-o-photo')
                                 ->schema([
                                     FileUpload::make('image')
                                         ->disk('public')
+                                        ->required()
                                         ->directory('posts')
-                                        ->image(),
+                                        ->image()
+                                        ->validationMessages([
+                                            'required' => 'Image wajib diupload.',
+                                        ]),
                                 ]),
 
                             Section::make('Meta Information')
